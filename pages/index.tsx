5dde949a9104865a21bb8be85824f2e200460bda
Jonathan <jwmp001@gmail.com>
2022-12-21 14:23:12 -0500

    Added inital search functionality to the Google Search Button.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,24 @@ import Footer from '../components/footer'
 import { MicrophoneIcon, Squares2X2Icon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import { useRef } from 'react'
+import { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter()
+  const searchInputRef = useRef(null)
+
+  const search = (e) => {
+    e.preventDefault()
+    const term = searchInputRef.current.value
+
+    if (!term) return
+
+    router.push(`/search?${term}`)
+  }
+
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <Head>
@@ -49,13 +63,17 @@ export default function Home() {
 
         <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
           <MagnifyingGlassIcon className='h-5 mr-3 text-gray-500' />
-          <input type="text" className='flex-grow focus:outline-none' />
+          <input 
+            ref={searchInputRef} 
+            type="text" 
+            className='flex-grow focus:outline-none' 
+          />
           <MicrophoneIcon className='h-5' />
         </div>
 
         <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'>
-          <button className='btn'>Google Search</button>
-          <button className='btn'>I'm Feeling Lucky</button>
+          <button onClick={search} className='btn'>Google Search</button>
+          <button onClick={search} className='btn'>I'm Feeling Lucky</button>
         </div>
       </form>
 
