7eba8c299bd675b728a240dda2771527e65f1f7d
Jonathan <jwmp001@gmail.com>
2022-12-21 23:53:58 -0500

    Added className prop and assigned the className properties to className variable.

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 type Props = {
     url: string;
+    className: string;
 }
 
-function avatar({url}: Props) {
+function avatar({url, className}: Props) {
   return ( 
     <img 
-        className='h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110'
+        className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
         loading='lazy'
         src={url}
         alt='avatar picture'
