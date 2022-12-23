2c85848003130184bad7ac25a6f9685dbd1007c1
Jonathan <jwmp001@gmail.com>
2022-12-22 22:57:53 -0500

    Added a server side rendering function to use Dummy Data or the Google Search API.

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
 import Header from '../components/header'
+import Response from '../response'
 
-type Props = {}
+type Props = {
+  results: string;
+}
 
-function Search({}: Props) {
+function Search({ results }: Props) {
+  console.log(results)
   return (
     <div>
         <Head>
@@ -14,9 +18,23 @@ function Search({}: Props) {
 
         {/* Header */}
         <Header />
+
         {/* Search Results */}
     </div>
   )
 }
 
 export default Search
+
+export async function getServerSideProps (context) {
+    const useDummyData = true; // Set to true to use DummyData instead of the Google API (100 search limit per day)
+
+    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}`).then(response => response.json())
+
+    // Passes Results to Client after the Server has rendered
+    return {
+      props: {
+        results: data,
+      }
+    }
+}
