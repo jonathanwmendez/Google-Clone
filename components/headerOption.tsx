b1c3b5c7ce3f0707feb33f8c9e1d3b0f79a8ee10
Jonathan <jwmp001@gmail.com>
2022-12-22 21:16:45 -0500

    Finished creating the Header components for the Google Search Results page.

--- components/headerOption.tsx
+++ components/headerOption.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-type Props = {}
+type Props = {
+    Icon: string;
+    title: string;
+    selected: boolean;
+}
 
-function HeaderOption({}: Props) {
+function HeaderOption({ Icon, title, selected }: Props) {
   return (
-    <div>HeaderOption</div>
+    <div className={`flex items-center space-x-1 border-b-4 border-transparent hover:text-blue-500 hover:border-blue-500 pb-3 cursor-pointer ${selected && 'text-blue-500 border-blue-500'}`}>
+        <Icon className='h-4' />
+        <p className='hidden sm:inline-flex'>{title}</p>
+    </div>
   )
 }
 
--- components/headerOptions.tsx
+++ components/headerOptions.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import HeaderOption from './headerOption'
+import { MagnifyingGlassIcon, PhotoIcon, PlayCircleIcon, TagIcon, BookmarkSquareIcon, EllipsisVerticalIcon } from '@heroicons/react/24/outline'
 
 type Props = {}
 
 function HeaderOptions({}: Props) {
   return (
-    <div>
+    <div className='flex w-full text-gray-700 justify-evenly text-sm lg:text-base lg:justify-start lg:space-x-36 lg:pl-52 border-b-[1px]'>
         {/* Left */}
+        <div className='flex space-x-6'>
+            <HeaderOption Icon={MagnifyingGlassIcon} title='All' selected />
+            <HeaderOption Icon={PhotoIcon} title='Images' />
+            <HeaderOption Icon={PlayCircleIcon} title='Videos' />
+            <HeaderOption Icon={TagIcon} title='Shopping' />
+            <HeaderOption Icon={BookmarkSquareIcon} title='Books' />
+            <HeaderOption Icon={EllipsisVerticalIcon} title='More' />
+        </div>
 
         {/* Right */}
         <div className='flex space-x-4'>
