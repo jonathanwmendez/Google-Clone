874ce0b02d336f69fbe51d4756e6352dd42ecdeb
Jonathan <jwmp001@gmail.com>
2022-12-22 23:12:40 -0500

    Created a Search Results component and imported the component into the Search page.

---
+++ components/searchResults.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    results: string;
+}
+
+function SearchResults({ results }: Props) {
+  return (
+    <div>SearchResults</div>
+  )
+}
+
+export default SearchResults
--- pages/search.tsx
+++ pages/search.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import Head from 'next/head'
 import Header from '../components/header'
 import Response from '../response'
+import { useRouter } from 'next/router'
+import SearchResults from '../components/searchResults'
 
 type Props = {
   results: string;
 }
 
 function Search({ results }: Props) {
+  const router = useRouter()
+
   console.log(results)
   return (
     <div>
         <Head>
-            <title>Google Search</title>
+            <title>{router.query.term} - Google Search</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
@@ -20,6 +24,8 @@ function Search({ results }: Props) {
         <Header />
 
         {/* Search Results */}
+        <SearchResults results={results} />
+
     </div>
   )
 }
@@ -28,8 +34,9 @@ export default Search
 
 export async function getServerSideProps (context) {
     const useDummyData = true; // Set to true to use DummyData instead of the Google API (100 search limit per day)
+    const startIndex = context.query.start || '0'
 
-    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}`).then(response => response.json())
+    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(response => response.json())
 
     // Passes Results to Client after the Server has rendered
     return {
